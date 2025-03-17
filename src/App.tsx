0eca3705f856130e7abd939003f8112b138e2433
Calvin Hardowar <36016757+CalvinHardowar1@users.noreply.github.com>
2025-03-16 21:36:41 -0400

    Update App.tsx
    added import for profile and route.

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Welcome from './Frontend/welcome';
 import Register from './Frontend/register'; 
+import Profile from "./Frontend/profile";
 
 function App() {
   return (
@@ -9,10 +10,10 @@ function App() {
       <Routes>
         <Route path="/" element={<Welcome />} />
         <Route path="/register" element={<Register />} />
+        <Route path="/profile" element={<Profile />} />
       </Routes>
     </Router>
   );
 }
 
 export default App;
-
