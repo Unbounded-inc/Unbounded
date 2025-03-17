4ec7fe960e0230a8ab3c67a3364b370bc87d458d
MReyes1241 <Manuelreyes1241@Outlook.com>
2025-03-17 01:33:46 -0400

    Routes: Added more routes to the site

--- src/frontend/src/routes/AppRoutes.tsx
+++ src/frontend/src/routes/AppRoutes.tsx
@@ -1,5 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Welcome from "../Pages/userPages/Login/welcome";
+import Register from "../Pages/userPages/Signup/register";
+import Profile from "../Pages/userPages/profile/profile";
 
 
 const AppRoutes = () => {
@@ -7,6 +9,8 @@ const AppRoutes = () => {
     <Router>
       <Routes>
         <Route path="/" element={<Welcome />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/profile" element={<Profile />} />
       </Routes>
     </Router>
   );
