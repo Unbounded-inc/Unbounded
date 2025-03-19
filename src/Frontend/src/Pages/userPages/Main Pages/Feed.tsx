d4d68d6e342785ebfccaf3e9e4deefb3b57ffe47
Roy Delgado <roy.delgado63@myhunter.cuny.edu>
2025-03-18 21:50:12 -0400

    created pages for navbar

---
+++ src/Frontend/src/Pages/userPages/Main Pages/Feed.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import '../../../Styles/Feed.css';
+
+const Feed: React.FC = () => {
+    return (
+    <div>
+       <p>Hello</p>
+    </div>
+    );
+};
+export default Feed;
--- src/frontend/src/routes/AppRoutes.tsx
+++ src/frontend/src/routes/AppRoutes.tsx
@@ -2,7 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Welcome from "../Pages/userPages/Login/welcome";
 import Register from "../Pages/userPages/Signup/register";
 import Profile from "../Pages/userPages/profile/profile";
-
+import Feed from "../Pages/userPages/Main Pages/Feed";
 
 const AppRoutes = () => {
   return (
@@ -11,6 +11,7 @@ const AppRoutes = () => {
         <Route path="/" element={<Welcome />} />
         <Route path="/register" element={<Register />} />
         <Route path="/profile" element={<Profile />} />
+        <Route path="/feed" element={<Feed />} />
       </Routes>
     </Router>
   );
