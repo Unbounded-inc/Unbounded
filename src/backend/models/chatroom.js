445c135aa045211c5d31ca4f5e419cd649817adc
Isabel Stec <47402788+isabelstec@users.noreply.github.com>
2025-04-06 17:37:30 -0400

    Update chatroom.js

--- src/backend/models/chatroom.js
+++ src/backend/models/chatroom.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const ChatRoomSchema = new mongoose.Schema({
   isGroup: { type: Boolean, default: false },
-  name: String, // Optional: for group chats
+  name: String,
   members: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
 });
 
