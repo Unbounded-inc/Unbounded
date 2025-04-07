8b14dae231bfe5912f177448bb1cc7f0ac940062
MReyes1241 <Manuelreyes1241@Outlook.com>
2025-04-06 21:24:21 -0400

    Maintenance: QOL

--- src/frontend/src/routes/AppRoutes.tsx
+++ src/frontend/src/routes/AppRoutes.tsx
@@ -3,8 +3,9 @@ import Welcome from "../Pages/userPages/Login/welcome";
 import Register from "../Pages/userPages/Signup/register";
 import Profile from "../Pages/userPages/profile/profile";
 import Feed from "../Pages/mainPages/Feed";
-import {NotFoundPage} from "../Pages/mainPages/NotFound.page.tsx";
+import { NotFoundPage } from "../Pages/mainPages/NotFound.page.tsx";
 import Messages from "../Pages/mainPages/Messages.tsx";
+import PrivateRoute from "../components/Auth/PrivateRoute.tsx"; // ✅ Import your wrapper
 
 const AppRoutes = () => {
   return (
@@ -12,9 +13,33 @@ const AppRoutes = () => {
       <Routes>
         <Route path="/" element={<Welcome />} />
         <Route path="/register" element={<Register />} />
-        <Route path="/profile" element={<Profile />} />
-        <Route path="/feed" element={<Feed />} />
-        <Route path="/messages" element={<Messages />} />
+
+        {/* ✅ Protected Routes */}
+        <Route
+          path="/profile"
+          element={
+            <PrivateRoute>
+              <Profile />
+            </PrivateRoute>
+          }
+        />
+        <Route
+          path="/feed"
+          element={
+            <PrivateRoute>
+              <Feed />
+            </PrivateRoute>
+          }
+        />
+        <Route
+          path="/messages"
+          element={
+            <PrivateRoute>
+              <Messages />
+            </PrivateRoute>
+          }
+        />
+
         <Route path="*" element={<NotFoundPage />} />
       </Routes>
     </Router>
